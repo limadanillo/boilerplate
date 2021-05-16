50beb69997482df4bafa6882cb4bd35eeb12845b
Danillo Lima <danillobiscuit03@gmail.com>
2021-05-16 15:45:51 -0300

    Add RTL config

--- src/components/main/test.tsx
+++ src/components/main/test.tsx
@@ -4,8 +4,9 @@ import Main from '.'
 
 describe('</Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
 
     expect(screen.getByRole('heading', { name: /react avançado/i })).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
